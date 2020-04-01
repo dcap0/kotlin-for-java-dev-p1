f21963e4f3977395133ea407155e3051ebc41cb0
Dennis Capone <capone@alamon.com>
2020-04-01 10:08:24 -0600

    Build passing all different letter tests

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -5,13 +5,23 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 fun evaluateGuess(secret: String, guess: String): Evaluation {
     var right: Int = 0
     var wrong: Int = 0
+    var rightCheck: Boolean = false
 
-    for (i in 0..secret.length){
-        if (guess[i] == secret[i]){
+    for(i in 0 until guess.length) {//iterate over the guess
+        if (guess[i] == secret[i]) {
             right++
+        }//confirms whether the current index of guess matches secret
+        else {
+            for (y in 0 until secret.length){
+                //check guess[i] against each letter in the secret.
+                if(guess[i] == secret[y] && secret[y] != secret[i]) {
+                    wrong++
+                }
+            }
         }
-        else {wrong++}
     }
+
+
 
     return Evaluation(right, wrong)
 }
