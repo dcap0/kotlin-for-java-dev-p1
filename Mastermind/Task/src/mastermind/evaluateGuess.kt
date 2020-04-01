8d907ec3156fa6eb10cc1b3f2733cb65c7ef0ef3
Dennis Capone <capone@alamon.com>
2020-04-01 15:52:54 -0600

    3 tests left

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -4,26 +4,30 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
     var secretMutable = secret
-    var replacementChar: Int = 0
+    var guessMutable = guess
+    var replacementCharS: Int = 0
+    var replacementCharG: Int = 1
+    var replacementCharH: Int = 2
+    var replacementCharI: Int = 3
     var right: Int = 0
     var wrong: Int = 0
-    println(secret)
 
 
 
-    for(i in 0 until guess.length) {//iterate over the guess, increments the right counter and replaces the value of secretMutable[i] to "0"
+    for(i in 0 until guess.length) {//for each index, iterate over, increment the counter, replace the letter of both secret and guess "cross both off"
         if (guess[i] == secretMutable[i]) {
             right++
-            secretMutable = secretMutable.replaceFirst(secretMutable[i], replacementChar.toChar())
-            println(secretMutable)
-        }//confirms whether the current index of guess matches secret
-        else {
-            for (y in 0 until guess.length){
-                if(guess[i] == secretMutable[y]) {
-                    wrong++
-                    secretMutable = secretMutable.replace(secretMutable[y],replacementChar.toChar())
-            }
-            }
+            secretMutable = secretMutable.replaceFirst(secretMutable[i], replacementCharS.toChar())//replace the letter in the secret
+            guessMutable = guessMutable.replaceFirst(guessMutable[i], replacementCharG.toChar())//replace the letter in the guess
+        }
+    }
+
+
+    for (y in 0 until guess.length){//for each item in the array
+        var echo = guessMutable[y]
+        if(secretMutable.contains(guessMutable[y])){//if the current item exists
+            wrong++//increment wrong counter
+            guessMutable = guessMutable.replace(guessMutable[y], replacementCharH.toChar())//replace the letter in the guess
         }
     }
 
