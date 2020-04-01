2655cc60fcf6b853ab075bdf1142d14a9ae36ed0
Dennis Capone <capone@alamon.com>
2020-04-01 16:37:48 -0600

    Cleaned up a little, failing 3 tests

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -7,12 +7,8 @@ fun evaluateGuess(secret: String, guess: String): Evaluation {
     var guessMutable = guess
     var replacementCharS: Int = 0
     var replacementCharG: Int = 1
-    var replacementCharH: Int = 2
-    var replacementCharI: Int = 3
     var right: Int = 0
     var wrong: Int = 0
-
-
 
     for(i in 0 until guess.length) {//for each index, iterate over, increment the counter, replace the letter of both secret and guess "cross both off"
         if (guess[i] == secretMutable[i]) {
@@ -22,16 +18,12 @@ fun evaluateGuess(secret: String, guess: String): Evaluation {
         }
     }
 
-
     for (y in 0 until guess.length){//for each item in the array
-        var echo = guessMutable[y]
         if(secretMutable.contains(guessMutable[y])){//if the current item exists
             wrong++//increment wrong counter
-            guessMutable = guessMutable.replace(guessMutable[y], replacementCharH.toChar())//replace the letter in the guess
+            guessMutable = guessMutable.replace(guessMutable[y], replacementCharG.toChar())//replace the letter in the guess
         }
     }
-
-
 
     return Evaluation(right, wrong)
 }
