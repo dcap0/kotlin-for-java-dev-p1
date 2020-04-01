73ccfe5cacdc55298bdba149e9ef258a36cbcea7
Dennis Capone <capone@alamon.com>
2020-04-01 13:52:04 -0600

    Passing 14 out of 26 repeatedLetters tests

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -3,20 +3,26 @@ package mastermind
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
+    var secretMutable = secret
+    var replacementChar: Int = 0
     var right: Int = 0
     var wrong: Int = 0
-    var rightCheck: Boolean = false
+    println(secret)
 
-    for(i in 0 until guess.length) {//iterate over the guess
-        if (guess[i] == secret[i]) {
+
+
+    for(i in 0 until guess.length) {//iterate over the guess, increments the right counter and replaces the value of secretMutable[i] to "0"
+        if (guess[i] == secretMutable[i]) {
             right++
+            secretMutable = secretMutable.replaceFirst(secretMutable[i], replacementChar.toChar())
+            println(secretMutable)
         }//confirms whether the current index of guess matches secret
         else {
-            for (y in 0 until secret.length){
-                //check guess[i] against each letter in the secret.
-                if(guess[i] == secret[y] && secret[y] != secret[i]) {
+            for (y in 0 until guess.length){
+                if(guess[i] == secretMutable[y]) {
                     wrong++
-                }
+                    secretMutable = secretMutable.replaceFirst(secretMutable[y],replacementChar.toChar())
+            }
             }
         }
     }
