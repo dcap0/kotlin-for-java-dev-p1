345ec223775084a22fee40635de707f0642f335f
Dennis Capone <capone@alamon.com>
2020-04-02 10:40:11 -0600

    It works

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -5,8 +5,7 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 fun evaluateGuess(secret: String, guess: String): Evaluation {
     var secretMutable = secret
     var guessMutable = guess
-    var replacementCharS: Int = 0
-    var replacementCharG: Int = 1
+
     var right: Int = 0
     var wrong: Int = 0
 
@@ -17,33 +16,17 @@ fun evaluateGuess(secret: String, guess: String): Evaluation {
     for(i in 0 until guess.length) {//for each index, iterate over, increment the counter, replace the letter of both secret and guess "cross both off"
         if (guess[i] == secretMutable[i]) {
             right++
-            secretMutable = secretMutable.replaceFirst(secretMutable[i], replacementCharS.toChar())//replace the letter in the secret
-            guessMutable = guessMutable.replaceFirst(guessMutable[i], replacementCharG.toChar())//replace the letter in the guess
+            secretMutable = secretMutable.replaceFirst(secretMutable[i], 'S')//replace the letter in the secret
+            guessMutable = guessMutable.replaceFirst(guessMutable[i], 'G')//replace the letter in the guess
         }
     }
 
-    //println(guessMutable+" guess")
-    //println(secretMutable+" secret")
-
-
-    for(y in 0 until guess.length){//match the current item in the guess to each item in the secret
-        if (secretMutable.contains(guessMutable[y]) && guessMutable[y]!= replacementCharG.toChar()) { //if secret contains the current iteration in guess and is not the replacement value
-            var echo = guessMutable[y]//set the current guess to a variable.
-            var repeats: Int = guessMutable.count{ it == echo }//get the number of repeats in the guess.
-            wrong += repeats//increment the counter by the number of repeats.
-            for(x in 0 until guess.length){//change every value equal to echo to the replacement character.
-                if(guessMutable[x] == echo){
-                    guessMutable = guessMutable.replace(guessMutable[x], replacementCharG.toChar())
-                }
-            }
+    for(y in 0 until guess.length){
+        if(secretMutable.contains(guessMutable[y])){
+            wrong++
+            secretMutable = secretMutable.replaceFirst(guessMutable[y], 'S')
         }
     }
-
-
-    //println(guessMutable+" guess")
-   // println(secretMutable+" secret")
-
-
 
     return Evaluation(right, wrong)
 }
